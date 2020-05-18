a9db93e0d73c5ac7999f2f2600d22b304b4d7902
Ojo Oluwasetemi <setemiojo@gmail.com>
2020-05-18 08:39:03 +0100

    🐛 FIX: fix the checkbox to check and uncheck

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 const checkboxes = document.querySelectorAll("input[type='checkbox']");
 const cards = Array.from(document.querySelectorAll('.cause-wrap'));
-console.log(cards);
-
-console.log(checkboxes);
 
 const checkboxesArray = Array.from(checkboxes);
 
-function handleCheck(e) {
-  console.log(e.currentTarget.id);
+// store the state of the checkbox
+const state = [];
 
+// handle check on
+function handleCheck(e) {
+  // store the selected in the state
+  state.push(e.currentTarget.id);
   cards.forEach(card => {
+    // just for one case
     const content = card.querySelector('p').textContent.toLowerCase();
     if (content !== e.currentTarget.id) {
       card.style.display = 'none';
     }
+    // check each card and restored the state
+    state.forEach(eachState => {
+      if (eachState === content) {
+        card.style.display = 'block';
+      }
+    });
 
-    // card.querySelector('p').textContent === e.currentTarget.id
-    //   ? (card.style.display = 'block')
-    //   : (card.style.display = 'node');
+    // handle unChecked
+    if (!e.target.checked) {
+      // the one that was
+      // select the card that match the checkbox that was clicked
+      if (content === e.currentTarget.id) {
+        card.style.display = 'none';
+      }
+    }
   });
 }
 
@@ -25,6 +38,6 @@ checkboxesArray.forEach(checkbox => {
   checkbox.addEventListener('input', handleCheck);
 });
 
-// things left to handle - restore the state of data after unchecked
-// handle keyboard event
-// handleCheck input for multiple
+// things left to handle - restore the state of data after unchecked ✅
+// handle keyboard event ✅
+// handleCheck input for multiple ✅
