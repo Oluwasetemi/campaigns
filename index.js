36dc444d84f23afca61c8353c38287b42a8f0b23
Ojo Oluwasetemi <setemiojo@gmail.com>
2020-05-18 10:52:22 +0100

    🐛 FIX: fix the issues with duplicate after testing

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const cards = Array.from(document.querySelectorAll('.cause-wrap'));
 const checkboxesArray = Array.from(checkboxes);
 
 // store the state of the checkbox
-const state = [];
+
+let state = [];
 
 // handle check on
 function handleCheck(e) {
-  // store the selected in the state
-  state.push(e.currentTarget.id);
+  // store the selected in the state and make sure it is only one entry and unique
+  if (e.currentTarget.id && !state.includes(e.currentTarget.id)) {
+    state.push(e.currentTarget.id);
+  }
   cards.forEach(card => {
     // just for one case
     const content = card.querySelector('p').textContent.toLowerCase();
@@ -26,10 +29,12 @@ function handleCheck(e) {
     // handle unChecked
     if (!e.target.checked) {
       // the one that was
-      // select the card that match the checkbox that was clicked
+      // select the card that match the checkbox that was unchecked
       if (content === e.currentTarget.id) {
         card.style.display = 'none';
       }
+      // remove the unchecked from the store
+      state = state.filter(eachState => eachState === e.currentTarget.id);
     }
   });
 }
